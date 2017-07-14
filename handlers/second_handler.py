f4ca08aa6a51b4d415e46c50a6590f3bbf22980e
radkins3 <radkins135@gmail.com>
2017-07-14 09:31:34 -0400

    dds

--- handlers/second_handler.py
+++ handlers/second_handler.py
@@ -14,5 +14,5 @@ class SecondHandler(webapp2.RequestHandler):
             "title": "Second Title",
             "content": "Goodbye"
         }
-        template = jinja_env.env.get_template('templates/tmpl.html')
+        template = jinja_env.env.get_template('templates/input.html')
         self.response.out.write(template.render(html_params))
