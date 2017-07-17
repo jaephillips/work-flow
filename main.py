6a81a9031321d04ffcfe626fac05e65edff14e0e
radkins3 <radkins135@gmail.com>
2017-07-17 10:20:10 -0400

    sdsda

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import logging
 from handlers import jinja_env
 from handlers import main_handler
 from handlers import second_handler
+from handlers import third_handler
 
 jinja_env.init(os.path.dirname(__file__))
 
 app = webapp2.WSGIApplication([
     ('/', main_handler.MainHandler),
     ('/second', second_handler.SecondHandler),
+    ('/formlist', third_handler.FormListHandler),
 ], debug=True)
